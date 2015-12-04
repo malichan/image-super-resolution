2030b0ee14c5a6adadea9f795206856c68734536
malichan <ke_ma@outlook.com>
2015-12-03 21:59:36 -0600

    Add global optimization

---
+++ global_optimize.m
@@ -0,0 +1,25 @@
+function [ image_high_opt ] = global_optimize( image_high, image_low )
+
+[height_high, width_high] = size(image_high);
+[height_low, width_low] = size(image_low);
+scale_factor = height_high / height_low;
+
+image_high_opt = zeros(height_high, width_high);
+for h = 1:height_low
+    for w = 1:width_low
+        h_offset = (h - 1) * scale_factor;
+        w_offset = (w - 1) * scale_factor;
+        px_high = image_high(h_offset+1:h_offset+scale_factor,...
+            w_offset+1:w_offset+scale_factor);
+        px_low = image_low(h, w);
+        
+        error = px_low - mean(mean(px_high, 1), 2);
+        px_high = px_high + error;
+        
+        image_high_opt(h_offset+1:h_offset+scale_factor,...
+            w_offset+1:w_offset+scale_factor) = px_high;
+    end
+end
+
+end
+
