d94e497019f5df54b2d12d252376028d78c85f7c
malichan <ke_ma@outlook.com>
2015-12-03 21:59:08 -0600

    Fix decompose / reconstruct

--- decompose_patch.m
+++ decompose_patch.m
@@ -1,14 +1,15 @@
-function [ patches ] = decompose_patch( image, patch_size )
+function [ patches ] = decompose_patch( image, patch_size, overlap_width )
 % Decompose an image into patches
 % Input:  image - image matrix
 %         patch_size - patch size
+%         overlap_width - overlap width
 % Output: patches - list of patches
 
 [height, width] = size(image);
-npatch_v = ceil((height - 1) / (patch_size - 1));
-npatch_h = ceil((width - 1) / (patch_size - 1));
-height_padded = npatch_v * (patch_size - 1) + 1;
-width_padded = npatch_h * (patch_size - 1) + 1;
+npatch_v = ceil((height - overlap_width) / (patch_size - overlap_width));
+npatch_h = ceil((width - overlap_width) / (patch_size - overlap_width));
+height_padded = npatch_v * (patch_size - overlap_width) + overlap_width;
+width_padded = npatch_h * (patch_size - overlap_width) + overlap_width;
 
 image_padded = zeros(height_padded, width_padded);
 image_padded(1:height, 1:width) = image;
@@ -22,8 +23,8 @@ end
 patches = zeros(patch_size * patch_size, npatch_v * npatch_h);
 for i = 1:npatch_v
     for j = 1:npatch_h
-        h_offset = (i - 1) * (patch_size - 1);
-        w_offset = (j - 1) * (patch_size - 1);
+        h_offset = (i - 1) * (patch_size - overlap_width);
+        w_offset = (j - 1) * (patch_size - overlap_width);
         patch = image_padded(h_offset+1:h_offset+patch_size,...
             w_offset+1:w_offset+patch_size);
         patches(:, (i - 1) * npatch_h + j) = patch(:);
--- reconstruct_patch.m
+++ reconstruct_patch.m
@@ -1,23 +1,24 @@
-function [ image ] = reconstruct_patch( patches, image_size )
+function [ image ] = reconstruct_patch( patches, image_size, overlap_width )
 % Reconstruct patches into an image
 % Input:  patches - list of patches
 %         image_size - size of image matrix
+%         overlap_width - overlap width
 % Output: image - image matrix
 
 height = image_size(1);
 width = image_size(2);
 patch_size = sqrt(size(patches, 1));
-npatch_v = ceil((height - 1) / (patch_size - 1));
-npatch_h = ceil((width - 1) / (patch_size - 1));
-height_padded = npatch_v * (patch_size - 1) + 1;
-width_padded = npatch_h * (patch_size - 1) + 1;
+npatch_v = ceil((height - overlap_width) / (patch_size - overlap_width));
+npatch_h = ceil((width - overlap_width) / (patch_size - overlap_width));
+height_padded = npatch_v * (patch_size - overlap_width) + overlap_width;
+width_padded = npatch_h * (patch_size - overlap_width) + overlap_width;
 
 sum = zeros(height_padded, width_padded);
 count = zeros(height_padded, width_padded);
 for i = 1:npatch_v
     for j = 1:npatch_h
-        h_offset = (i - 1) * (patch_size - 1);
-        w_offset = (j - 1) * (patch_size - 1);
+        h_offset = (i - 1) * (patch_size - overlap_width);
+        w_offset = (j - 1) * (patch_size - overlap_width);
         patch = reshape(patches(:, (i - 1) * npatch_h + j),...
             [patch_size, patch_size]);
         for h = 1:patch_size
